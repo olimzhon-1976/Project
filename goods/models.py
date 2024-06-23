38149ff570b34b53d10846dfa0d1ec04b68beaed
Olimxhon <olim_1976_2@mail.ru>
2024-06-23 16:12:30 +0300

    Admin panel setup

--- goods/models.py
+++ goods/models.py
@@ -1,5 +1,6 @@
 from tabnanny import verbose
 from django.db import models
+from django.urls import reverse
 
 
 class Categories(models.Model):
@@ -36,8 +37,13 @@ class Products(models.Model):
     def __str__(self):
         return f"{self.name} - Количестве {self.quantity}"
     
+    def get_absolute_url(self):
+        return reverse("catalog:product", kwargs={"product_slug": self.slug})
+    
+    
     def display_id(self):
         return f"{self.id:05}"
+
     
     def sell_price(self):
         if self.discount:
